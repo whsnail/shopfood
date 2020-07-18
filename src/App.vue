02d96830bd8ed4f0b900d6d725786cf4ceef59b4
whsnail <2812722145@qq.com>
2020-07-18 16:08:14 +0800

    init shopfood

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!--  -->
+<template>
+  <div class="app"> 
+    <router-view></router-view>
+    <footer-guider></footer-guider>
+  </div>
+</template>
+
+<script>
+import FooterGuider from './components/FooterGuider/FooterGuider.vue'
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {
+    FooterGuider
+  },
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style lang="stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
